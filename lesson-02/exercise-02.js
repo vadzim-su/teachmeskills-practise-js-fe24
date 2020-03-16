93f6a3f598993987bd0e2f727b33d66c9f425f81
Evgeny Svirsky <evgeny@cere.io>
2020-03-17 00:50:06 +0300

    Fixed exercise numbers.

--- lesson-02/exercise-02.js
+++ lesson-02/exercise-02.js
@@ -1,4 +1,4 @@
-// Exercise 07: Create function generator to iterate given array.
+// Exercise 02: Create function generator to iterate given array.
 function *arrayIterator(arr) {
   // Solution here...
 }
--- lesson-02/exercise-03.js
+++ lesson-02/exercise-03.js
@@ -1,1 +1,1 @@
-// Exercise 08: Implement rates calculator. There are 2 selects - with source rate and destination rate, input for sum.Rates should be in the array. It should not be a possibility to choose the same currencies.
+// Exercise 03: Implement rates calculator. There are 2 selects - with source rate and destination rate, input for sum.Rates should be in the array. It should not be a possibility to choose the same currencies.
